8cbf7314780473102532da1c94fa998401acfb2a
Christopher Curtis <ccurtisj@gmail.com>
2013-12-08 14:09:34 -0600

    Initial commit, runner configured

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaperTicketRunner::Application.config.secret_key_base = '7d7dc6991ee83d7c60240f1711ae4ae6f24b83ffc8297a158c660c1015b9658f2b85c65301ddf9923f12cf96f8a9378ad2f817728417d03428b7a8aa60b4d417'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PaperTicketRunner::Application.config.session_store :cookie_store, key: '_paper_ticket_runner_session'
